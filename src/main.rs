aaf7c0024a7ca7b506ff2a54d28b149d1b8bb223
Hiury Joaquim Oliveira André Araújo <hiuryo1996@gmail.com>
2024-03-28 21:45:24 -0300

    LUCRO MÁXIMO DA AÇÃO

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_profit() {
+        let shares_prices_1 = vec![7, 1, 5, 3, 6, 4];
+        assert_eq!(max_profit(shares_prices_1), 7);
+
+        let shares_prices_2 = vec![1, 2, 3, 4, 5];
+        assert_eq!(max_profit(shares_prices_2), 4);
+
+        let shares_prices_3 = vec![5, 4, 3, 2, 1];
+        assert_eq!(max_profit(shares_prices_3), 0);
+    }
+}
+
+fn max_profit(prices: Vec<i32>) -> i32 {
+    let mut max_profit = 0;
+    for i in 1..prices.len() {
+        if prices[i] > prices[i - 1] {
+            max_profit += prices[i] - prices[i - 1];
+        }
+    }
+    max_profit
+}
+
+fn main() {
+    let shares_prices = vec![7, 6, 4, 3, 1];
+    let max_profit = max_profit(shares_prices);
+    println!("Max profit: {}", max_profit);
+}
+
